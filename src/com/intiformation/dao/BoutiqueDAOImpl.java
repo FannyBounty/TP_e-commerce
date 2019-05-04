09bfa7069214ff7e576948f3434e20455734e8af
IN-MP-019 <IN-MP-019@192.168.1.14>
2019-05-04 16:07:42 +0200

    Annotations pour classes implémentation DAO

--- src/com/intiformation/dao/BoutiqueDAOImpl.java
+++ src/com/intiformation/dao/BoutiqueDAOImpl.java
@@ -2,9 +2,15 @@ package com.intiformation.dao;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.intiformation.entity.Categorie;
 import com.intiformation.entity.Client;
 import com.intiformation.entity.Commande;
+import com.intiformation.entity.Panier;
 import com.intiformation.entity.Produit;
 import com.intiformation.entity.User;
 
@@ -13,94 +19,122 @@ import com.intiformation.entity.User;
  * @author IN-MP-019
  *
  */
+@Repository
 public class BoutiqueDAOImpl implements IBoutiqueDAO {
+	
+	// déclaration de la session factory d'hibernate
+	@Autowired
+	private SessionFactory sf;
+	
+	/**
+	 * Setter pour injection spring
+	 * @param sf
+	 */
+	public void setSessionFactory(SessionFactory sf) {
+		this.sf = sf;
+	}
 
+	@Transactional
 	@Override
 	public Long ajouterCategorie(Categorie cat) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public List<Categorie> listeCategories() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public Categorie trouverCategorie(Long idCat) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional
 	@Override
 	public void supprimerCategorie(Long idCat) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
 	public void modifierCategorie(Long idCat) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
 	public Long ajouterProduit(Produit prod, Long idCat) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOly=true)
 	@Override
 	public List<Produit> listerProduits() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public List<Produit> trouverProduitsParMotCle(String mc) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public List<Produit> trouverProduitsParCategorie(Long idCat) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public List<Produit> trouverProduitsSelectionnes() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public Produit trouverProduit(Long idProd) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional
 	@Override
 	public void supprimerProduit(Long idProd) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
 	public void modifierProduit(Produit prod) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
 	public void ajouterUser(User user) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
-	public Commande enregistrerCommande(GestionPanier p, Client c) {
+	public Commande enregistrerCommande(Panier p, Client c) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/com/intiformation/dao/BoutiqueMetierImpl.java
+++ src/com/intiformation/dao/BoutiqueMetierImpl.java
@@ -2,6 +2,11 @@ package com.intiformation.dao;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.intiformation.entity.Categorie;
 import com.intiformation.entity.Client;
 import com.intiformation.entity.Commande;
@@ -10,80 +15,106 @@ import com.intiformation.entity.Produit;
 import com.intiformation.entity.Role;
 import com.intiformation.entity.User;
 
+@Repository
 public class BoutiqueMetierImpl implements IAdminCategoriesMetier {
+	
+	// déclaration de la session factory d'hibernate
+	@Autowired
+	private SessionFactory sf;
+	
+	/**
+	 * Setter pour injection spring
+	 * @param sf
+	 */
+	public void setSessionFactory(SessionFactory sf) {
+		this.sf = sf;
+	}
 
+	@Transactional
 	@Override
 	public void supprimerProduit(Long idProd) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
 	public void modifierProduit(Produit prod) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
 	public Long ajouterCategorie(Categorie cat) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public List<Categorie> listeCategories() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public Categorie trouverCategorie(Long idCat) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional
 	@Override
 	public void modifierCategorie(Long idCat) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
 	public Long ajouterProduit(Produit prod, Long idCat) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public List<Produit> listerProduits() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public List<Produit> trouverProduitsParMotCle(String mc) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public List<Produit> trouverProduitsParCategorie(Long idCat) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public List<Produit> trouverProduitsSelectionnes() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public Produit trouverProduit(Long idProd) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional
 	@Override
 	public Commande enregistrerCommande(Panier p, Client c) {
 		// TODO Auto-generated method stub
@@ -96,18 +127,21 @@ public class BoutiqueMetierImpl implements IAdminCategoriesMetier {
 		
 	}
 
+	@Transactional
 	@Override
 	public void modifierCategorie(Categorie cat) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
 	public void ajouterUser(User user) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
 	public void attribuerRole(Role role, Long userID) {
 		// TODO Auto-generated method stub
